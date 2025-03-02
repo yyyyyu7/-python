c75ee7874f28a152b6aec175b8a1f8f57de33f6e
yyyyyu7 <3468303753@qq.com>
2025-03-02 23:16:01 +0800

    Add files via upload
    一个简单的关于姓名和年龄的判断代码

---
+++ 年龄.py
@@ -0,0 +1,10 @@
+name=input("请输入你的名字")
+print("hello",name)
+age=int(input("请输入你的年龄"))
+if age<=18:
+    print("你还是一个学生哦，请好好学习")
+elif age<=60:
+    print("你是一位中年人哦，要好好上班哦")
+else:
+    print("你是一位老年人哦，请好好注意休息")
+    
