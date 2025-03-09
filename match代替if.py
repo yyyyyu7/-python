7bf23a250e92650df9c4cbcb17906a484c8fd713
yyyyyu7 <3468303753@qq.com>
2025-03-09 19:57:20 +0800

    Add files via upload
    循环代替

---
+++ match代替if.py
@@ -0,0 +1,13 @@
+score="B"
+
+match score:
+    case "A":
+        print("score is A.")
+    case "B":
+        print("score is B.")
+    case "C":
+        print("score is C.")
+    case _:
+        print("score is ???.")
+        
+
